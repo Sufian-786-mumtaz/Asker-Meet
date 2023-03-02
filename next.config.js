4699df6526d54c7419e3afbf01b9dd67dc53887a
Sufian Mumtaz <sufyan@saldir.com>
2023-03-02 13:02:56 +0500

    second

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withPlugins = require('next-compose-plugins');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withTM = require('next-transpile-modules')(['@jitsi/react-sdk']);
 module.exports = withPlugins([withTM],{
+  experimental: {
+    forceSwcTransforms: true,
+  },
     trailingSlash: false,
     exportPathMap: async function (
         defaultPathMap,
