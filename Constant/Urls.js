df22af4ff69d7f259e37c0133d480970ac11e95f
Sufian Mumtaz <sufyan@saldir.com>
2023-03-02 14:48:19 +0500

    fix

--- Constant/Urls.js
+++ Constant/Urls.js
@@ -1,6 +1,6 @@
 const Urls = {
     baseUrl: "wss://df3c-119-73-117-15.ngrok.io",
-    httpBaseUrl: "https://asker-meet.vercel.app/"
+    httpBaseUrl: "http://df3c-119-73-117-15.ngrok.io"
     
 }
 export default Urls
